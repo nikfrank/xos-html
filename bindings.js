53533468392aed935083fb4121d6a2c7f34d5fa3
nik frank <nikbfrank@gmail.com>
2014-12-18 22:21:14 +0200

    binding debug

--- bindings.js
+++ bindings.js
@@ -1,18 +1,15 @@
-// write bindings for the onclick the user will write for the confirm button
-
 document.addEventListener("DOMContentLoaded", function() {
 
-// bind to the expected inputs and the div
+    var eli = function(e){
 
+	setTimeout(function(){
+	    var price = (1*(document.getElementById('total-price').value))||0;
+	    var percent = (1*(document.getElementById('percent-tip').value))||0;
 
-    document.getElementById("texty").addEventListener("keydown", function(e){
+	    document.getElementById('tip-total').innerHTML = 'tip: $' + price*percent/100;
+	},50);
+    };
 
-	var price = 1*document.getElementById('total-price').value||0;
-	var percent = 1*document.getElemtentById('percent-tip').value||0;
-
-	document.getElementById('tip-total').innerHTML = 'tip: $' + price*percent
-
-    });
-
-
+    document.getElementById("percent-tip").addEventListener("keydown", eli);
+    document.getElementById("total-price").addEventListener("keydown", eli);
 });
